88495ec507731cf9bf3d7aac4057bed18a83787b
satoyuki <yuki01377@gmail.com>
2020-12-13 04:35:51 +0900

    get機能追加

---
+++ lambdas/common/Dynamo.js
@@ -0,0 +1,21 @@
+const AWS = require('aws-sdk');
+const documentClient = new AWS.DynamoDB.DocumentClient();
+
+const Dynamo = {
+  get: async (id, TableName) => {
+    const params = {
+      TableName,
+      Key: { id }
+    };
+    const data = await documentClient
+      .get(params)
+      .promise();
+    
+    if (!data || !data.Item) {
+      throw Error(`${TableName}に${id}がありません`);
+    }
+    
+    return data.Item;
+  }
+};
+module.exports = Dynamo;
---
+++ lambdas/endpoints/getKeijibanPosts.js
@@ -0,0 +1,23 @@
+const Responses = require('../common/API_Responses');
+const Dynamo = require('../common/Dynamo');
+const TableName = process.env.tableName;
+
+exports.handler = async (event, context, callback) => {
+  console.log('event', event);
+
+  if (!event.pathParameters || !event.pathParameters.id) {
+    // idが渡されて無い
+    return Responses._400({ message: 'idを指定してください' });
+  }
+  const id = event.pathParameters.id;
+  const res = await Dynamo.get(id, TableName).catch(err => {
+    console.log('error =', err);
+    return null
+  });
+
+  // idが存在しない時
+  if (!res) {    
+    return Responses._400({ message: '指定したidのデータは存在しません' });
+  }
+  return Responses._200(res);
+}
